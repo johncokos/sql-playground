7e9315c879457a28ad4c74c68d88d262c3c59cc5
John Cokos <highlander.44@gmail.com>
2019-10-25 14:28:43 -0700

    changed docs

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,9 @@ const Header = (props) => {
     <header>
       <h1>SQL Playground</h1>
       <p>This table is named <strong>shapes</strong> and has 2 columns: <strong>shape</strong> and <strong>color</strong> and supports SELECT, INSERT, UPDATE Statements (no ';' chaining)</p>
-      <p>Reload the page to refresh the database.</p>
+      <p>Reload the page to refresh/randomize the database.</p>
     </header>
-  )
+  );
 
 };
 
